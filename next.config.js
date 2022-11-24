4ee0f308efc60106fd43467992cafb5e8d68439a
Simon Nußbaumer <770632+Ponjimon@users.noreply.github.com>
2022-11-24 09:57:18 +0000

    chore: upgrade nextjs 13

--- next.config.js
+++ next.config.js
@@ -1,60 +1,8 @@
-const nextOffline = require('next-offline');
-
-const config = {
-  swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev) {
-      config.devtool = 'source-map';
-    }
-    if (isServer) {
-      config.resolve.mainFields.reverse();
-    }
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
 };
 
-const offlineConfig = [
-  nextOffline,
-  {
-    transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-    workboxOpts: {
-      swDest: 'static/service-worker.js',
-    },
-  },
-];
-
-const compose = plugins => ({
-  webpack(config, options) {
-    return plugins.reduce((config, plugin) => {
-      if (plugin instanceof Array) {
-        const [_plugin, ...args] = plugin;
-        plugin = _plugin(...args);
-      }
-      if (plugin instanceof Function) {
-        plugin = plugin();
-      }
-      if (plugin && plugin.webpack instanceof Function) {
-        return plugin.webpack(config, options);
-      }
-      return config;
-    }, config);
-  },
-
-  webpackDevMiddleware(config) {
-    return plugins.reduce((config, plugin) => {
-      if (plugin instanceof Array) {
-        const [_plugin, ...args] = plugin;
-        plugin = _plugin(...args);
-      }
-      if (plugin instanceof Function) {
-        plugin = plugin();
-      }
-      if (plugin && plugin.webpackDevMiddleware instanceof Function) {
-        return plugin.webpackDevMiddleware(config);
-      }
-      return config;
-    }, config);
-  },
-});
-
-module.exports = compose([config, offlineConfig]);
+module.exports = nextConfig;
